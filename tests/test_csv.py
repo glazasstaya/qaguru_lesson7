772b18898b6b3a82b5b1361d4b831f24dbbbccf4
Tatyana Sgibneva <sgibnevatata@gmail.com>
2023-04-27 15:31:49 +0300

    написала функцию для пути и тесты для csv и xls

---
+++ tests/test_csv.py
@@ -0,0 +1,23 @@
+import csv
+import pytest
+from tests.funtions import resources_path
+
+# оформить в тест, добавить ассерты и использовать универсальный путь
+
+def test_csv_write_and_read():
+    file = resources_path('eggs.csv')
+    rows = []
+
+    with open(file, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
+        csvwriter.writerow(['Alex', 'Serj', 'Yana'])
+
+    with open(file) as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            rows.append(row)
+            print(row)
+
+    assert rows[0] == ['Anna', 'Pavel', 'Peter']
+
---
+++ tests/test_xls.py
@@ -0,0 +1,23 @@
+import xlrd
+import pytest
+from tests.funtions import resources_path
+#оформить в тест, добавить ассерты и использовать универсальный путь
+
+def test_xls_info():
+    file = resources_path('file_example_XLS_10.xls')
+    book = xlrd.open_workbook(file)
+    print(f'Количество листов {book.nsheets}')
+    print(f'Имена листов {book.sheet_names()}')
+    sheet = book.sheet_by_index(0)
+    print(f'Количество столбцов {sheet.ncols}')
+    print(f'Количество строк {sheet.nrows}')
+    print(f'Пересечение строки 9 и столбца 1 = {sheet.cell_value(rowx=0, colx=1)}')
+    # печать всех строк по очереди
+    for rx in range(sheet.nrows):
+        print(sheet.row(rx))
+
+    assert book.nsheets == 1
+    assert book.sheet_names()[0] == 'Sheet1'
+    assert sheet.ncols == 8
+    assert sheet.nrows == 10
+    assert sheet.cell_value(rowx=0, colx=1) == 'First Name'
